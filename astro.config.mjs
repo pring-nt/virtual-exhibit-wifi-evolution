c1f2a8c560700fa71d8b5a1269e2fc2c63c892db
nathan <natetrndd@gmail.com>
2026-07-06 23:50:32 +0800

    fix: site and base in astro.config.mjs not set to current repository owner and name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite'
-import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
-import icon from 'astro-icon';
-
 export default defineConfig({
   integrations: [mdx(), react(), icon()],
-  site: 'https://jrgo7.github.io',
-  base: 'virtual-exhibit-template',
+  site: 'https://pring-nt.github.io',
+  base: 'virtual-exhibit-wifi-generations',
   vite: {
     plugins: [tailwindcss()],
     resolve: { alias: { '@': '/src' } },
   },
 });
-
